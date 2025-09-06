f73d693ae7468b0aa0b8c546f2bda78d37a022cd
AveryTidgwell <averytidgwell@gmail.com>
2025-09-06 14:20:14 -0300

    Delete 32nd dolphins.R

--- 32nd dolphins.R
+++
@@ -1,5 +0,0 @@
-library(reticulate)
-use_python("/Library/Frameworks/Python.framework/Versions/3.13/bin/python3", required = TRUE)  # Optional if you want a specific interpreter
-
-py_run_file("32nd dolphins.py")
-
